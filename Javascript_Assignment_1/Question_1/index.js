72ffab4ab853383c4afbcedec52636b4fddcb3c1
Saurabh Puri <purisaurabh2885@gmail.com>
2024-02-16 15:02:45 +0530

    Add assignment one solution

---
+++ Javascript_Assignment_1/Question_1/index.js
@@ -0,0 +1,14 @@
+// change the name
+
+document.getElementById("name").innerHTML = "Saurabh"
+document.body.style.background = "blue";
+
+
+var a = 10
+var b = 20
+ add()
+ 
+function add(){
+ console.log(a+b)
+}
+
