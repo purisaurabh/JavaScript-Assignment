95a656b0c29b7409a86175f3a0889419a0b9957a
Saurabh Puri <purisaurabh2885@gmail.com>
2024-02-22 22:53:26 +0530

    update assignment 3 solution

--- Javascript_Assignment_3/Question_3/index.js
+++ Javascript_Assignment_3/Question_3/index.js
@@ -1,8 +1,9 @@
 // Can you use return instead of break in loops?
 
 /*
-    Yes we can use the return instead of break in the javascript
-    to execute the function or loop completely
+   If you use return instead of break in a loop, it will exit the loop, but it will also exit the function entirely.
+   Whereas, if you use break, it will only exit the loop, allowing the function to continue executing. 
+   So consider the function necessity of function execution while you use return in place of break.
 */
 
 
