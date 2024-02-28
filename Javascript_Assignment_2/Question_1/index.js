b7b16f81d442a6a292853248decf705990d9a25c
Saurabh Puri <purisaurabh2885@gmail.com>
2024-02-28 09:57:27 +0530

    update assignment 2 solution

--- Javascript_Assignment_2/Question_1/index.js
+++ Javascript_Assignment_2/Question_1/index.js
@@ -9,8 +9,13 @@
 // for the let
 let a;
 console.log("The value before assign" , a);
+// Output : The value before assign undefined
+
 a = "Immanuel"
 console.log("The value after the assign" , a);
+// Output : The value after the assign Immanuel
+
+
 
 
 
@@ -27,6 +32,7 @@ console.log("The vaue of b if we are using the const", b);
 
 const c = 100
 console.log("The value c using the const ", c);
+// Output : The value c using the const  100
 
 
 // type of operator
@@ -34,32 +40,62 @@ console.log("The value c using the const ", c);
 // 1) Number
 let num = 1
 console.log("The type of num", typeof num);
+// Output : The type of num number
+
+
 let NaNvalue = NaN
 console.log("The type of NaNvalue" , typeof NaNvalue);
+// Output : The type of NaNvalue number
+
+
 // 2) String
 let str = "saurabh"
 console.log("The type of str", typeof str);
+//Output : The type of str string
+
+
 // 3)Undefined
 let x
 console.log("The type of x is : ", typeof x);
+// Output : The type of x is :  undefined
+
+
 // 4) Boolean
 let flag = true
 console.log("The tyep of the flag is :", typeof flag);
+// Output : The tyep of the flag is : boolean
+
+
 // 5) Object
 let z = null
 console.log("The type of the z is :", typeof z);
+// Output : The type of the z is : object
+
+
 let user = {
     Name : "Immanuel",
     Email:"thisistheemail@gmail.com"
 }
 console.log("The type of the user is :", typeof user);
+// Output : The type of the user is : object
+
+
+
+
 //6) Symbol
 console.log("The type is :", typeof Symbol());
+// Output : The type is : symbol
+
+
 //7) Array
 let arr = ["name" , "1" , null , NaN, true]
 console.log("The type of array :", typeof arr);
+// Output : The type of array : object
+
+
 //8 Date
 // Get the Current Time
 const date = new Date();
 console.log("The value :", date);
 console.log("The type is : " , typeof date);
+// Output : The type is :  object
